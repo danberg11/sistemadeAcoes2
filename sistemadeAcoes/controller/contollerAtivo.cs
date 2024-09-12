72e31169d6e27163924646d67787003276c3a773
danberg11 <Pn7931380@gmail.com>
2024-09-12 12:01:44 -0300

    Adicionar arquivos de projeto.

---
+++ sistemadeAcoes/controller/contollerAtivo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistemadeAcoes.controller
+{
+    internal class contollerAtivo
+    {
+        public void CadastrarAtivo()
+        {
+
+        }
+        public void DeletarAtivo() { 
+        
+        }
+
+        public void AlterarAtivo()
+        {
+
+        }
+
+        public void VisualizarCodigoAtivo()
+        {
+        
+        }
+
+        public static BindingSource VisualizarNomeAtivo()
+        {
+            return 0;s
+        }
+    }
+}
---
+++ sistemadeAcoes/model/conexao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistemadeAcoes.model
+{
+    internal class conexao
+    {
+        public static string Conectar()
+        {
+            return @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\daniel.bnoliveira\source\repos\sistemadeAcoes\sistemadeAcoes\dados\bdAcoes.mdf;Integrated Security=True";
+        }
+    }
+}
